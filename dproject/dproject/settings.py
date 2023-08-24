535ad22b0104208f87f24904a8f30b456a017a62
Daniel <daniel768galeano@gmail.com>
2023-08-24 18:30:02 -0400

    Configuraciones

--- dproject/dproject/settings.py
+++ dproject/dproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig', # Apéndice
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+
